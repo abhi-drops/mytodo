9c7fd636caf3414f9459988afc1f4f2c87b9a05e
abhi-drops <abhinand7667@gmail.com>
2024-10-10 14:56:14 +0530

    pwa bug

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: 'Vite PWA Project',
-        short_name: 'Vite PWA Project',
+        name: 'Plan It Out',
+        short_name: 'Plan It Out',
         theme_color: '#ffffff',
         icons: [
             {
